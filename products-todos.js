c643c69ea1d14611dbf7d8b5fd90ce00f5818ab6
Gabriel <gabrielfunes100202@gmail.com>
2022-11-25 17:12:03 -0300

    Creación de productos dinamicos en "todos"

---
+++ products-todos.js
@@ -0,0 +1,56 @@
+const productoHTML = (nombre, precio, img) => {
+const product = `
+        <div class="product__filtro">
+            <a class="filtro__link" href="#"><i class="fa-solid fa-trash filtro__ico"></i></a>                
+            <a class="filtro__link" href="#"><i class="fa-solid fa-pencil filtro__ico"></i></a>
+        </div>
+        <img class="producto__img" src="../${img}">
+        <h3 class="producto__nombre">${nombre}</h3>
+        <h3 class="producto__precio">${precio}</h3>
+        </a>`
+    console.log(product);
+    return product;
+}
+
+const crearProducto = (nombre, precio, img) => {
+    const content = productoHTML(nombre, precio, img);
+    const div = document.createElement("div");
+    div.classList.add("sections__producto");
+    div.classList.add("product");
+    div.innerHTML = content;
+    return div;
+}
+
+const listaMundiales = () => {
+    return fetch("http://localhost:3000/productos").then((response) => response.json());
+}
+const listaHistoricos = () => {
+    return fetch("http://localhost:3000/historicos").then((response) => response.json());
+}
+const listaLigas = () => {
+    return fetch("http://localhost:3000/ligas").then((response) => response.json());
+}
+listaMundiales()
+    .then((data) => {
+    data.forEach((datos) => {
+        const nuevoDiv = crearProducto(datos["nombre"], datos["precio"], datos["imagen"]);
+        const container = document.querySelector(".product-todos");
+        container.appendChild(nuevoDiv);
+    })
+});
+listaHistoricos()
+    .then((data) => {
+    data.forEach((datos) => {
+        const nuevoDiv = crearProducto(datos["nombre"], datos["precio"], datos["imagen"]);
+        const container = document.querySelector(".product-todos");
+        container.appendChild(nuevoDiv);
+    })
+});
+listaLigas()
+    .then((data) => {
+    data.forEach((datos) => {
+        const nuevoDiv = crearProducto(datos["nombre"], datos["precio"], datos["imagen"]);
+        const container = document.querySelector(".product-todos");
+        container.appendChild(nuevoDiv);
+    })
+});
